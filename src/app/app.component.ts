7f837809a473f77a3d660a74de55f7a326aa8d07
Riya Mathew <riya.mathew@experionglobal.com>
2019-11-15 18:21:08 +0530

    Allinment Complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {MatSnackBar} from '@angular/material/snack-bar';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
+
 })
 export class AppComponent {
+  constructor(private http: HttpClient, private snackBar: MatSnackBar) { }
   title = 'git-hub-viewer';
+  userName: any;
+  data: any;
+  localData: any;
+  errmsg = false;
+
+  searching = false;
+  search() {
+this.localData = localStorage.getItem('this.username');
+if (this.userName == this.localData) {
+  return JSON.parse(localStorage.getItem('currentUser'));
+} else {
+    this.searching = true;
+    this.errmsg = false;
+    // tslint:disable-next-line: max-line-length
+    this.http.get(' https://api.github.com/users/' + this.userName + '?access_token=4230a32869b6ed09bb256138b67dcd06916260bc').subscribe(response => {
+      this.data = response;
+      this.searching = false;
+      this.errmsg = false;
+
+      localStorage.setItem(this.userName, JSON.stringify(this.data));
+      console.log(this.data);
+    },
+    err => {
+      this.data = false;
+      this.searching = false;
+      this.errmsg = true;
+      this.snackBar.open(err.error.message,'close', {
+        duration: 2000,
+        verticalPosition: 'top'
+      });
+   
+    }
+    );
+
+  }
+
+
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatButtonModule,
+    HttpClientModule,
+    FormsModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+    MatSnackBarModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
